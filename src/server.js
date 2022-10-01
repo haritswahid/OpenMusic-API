ed0152ed3e430e05c76ce03bf1cfc71154996594
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-01 22:58:37 +0700

    menghubungkan server dengan api playlist dan mendefinisikan jwt token

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const hapiAuthJwt = require('@hapi/jwt');
 
 const OpenMusicValidator = require('./validator/openmusic');
 const TokenManager = require('./tokenize/TokenManager');
@@ -10,17 +11,20 @@ const albums = require('./api/albums');
 const songs = require('./api/songs');
 const users = require('./api/users');
 const authentications = require('./api/authentications');
+const playlists = require('./api/playlists');
 
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
 const UsersService = require('./services/postgres/UsersService');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const PlaylistsService = require('./services/postgres/PlaylistsService');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const playlistsService = new PlaylistsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -29,6 +33,30 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: hapiAuthJwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('openmusic_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register(
@@ -60,6 +88,13 @@ const init = async () => {
           authenticationsService,
           usersService,
           tokenManager: TokenManager,
+          validator: OpenMusicValidator,
+        },
+      },
+      {
+        plugin: playlists,
+        options: {
+          service: playlistsService,
           validator: OpenMusicValidator,
         },
       },
