4d1763712fed98a986740ede3a5339223981571c
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-01 18:55:32 +0700

    mengimplementasi api users dan authentications

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+const OpenMusicValidator = require('./validator/openmusic');
+const TokenManager = require('./tokenize/TokenManager');
+
 const albums = require('./api/albums');
 const songs = require('./api/songs');
+const users = require('./api/users');
+const authentications = require('./api/authentications');
+
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
-// const NotesService = require('./services/inMemory/NotesService');
-const OpenMusicValidator = require('./validator/openmusic');
+const UsersService = require('./services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -34,6 +43,22 @@ const init = async () => {
         plugin: songs,
         options: {
           service: songsService,
+          validator: OpenMusicValidator,
+        },
+      },
+      {
+        plugin: users,
+        options: {
+          service: usersService,
+          validator: OpenMusicValidator,
+        },
+      },
+      {
+        plugin: authentications,
+        options: {
+          authenticationsService,
+          usersService,
+          tokenManager: TokenManager,
           validator: OpenMusicValidator,
         },
       },
