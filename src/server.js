ab8c8935f51b7bc4032184098bf28c65ccc248da
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-09 01:52:32 +0700

    Mengimplementasi event extensions onPreResponse

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ const CollaborationsService = require('./services/postgres/CollaborationsService
 const producerService = require('./services/rabbitmq/ProducerService');
 const StorageService = require('./services/storage/StorageService');
 const CacheService = require('./services/redis/CacheService');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const storageService = new StorageService(path.resolve(__dirname, 'api/albums/images'));
@@ -132,6 +133,35 @@ const init = async () => {
       },
     ],
   );
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
