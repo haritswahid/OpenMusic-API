aebf2401ecc07b62b39eca192df98c49d4d02629
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-08 23:58:02 +0700

    Menambakan route untu menangani request file statis

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const routes = (handler) => [
   {
     method: 'POST',
@@ -34,6 +36,15 @@ const routes = (handler) => [
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream',
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/albums/cover/{param*}',
+    handler: {
+      directory: {
+        path: path.resolve(__dirname, 'images'),
       },
     },
   },
